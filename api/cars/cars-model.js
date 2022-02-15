d89596dba216ea989f1b61d1152bfea5accf09e5
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-15 11:54:50 -0800

    updated migrations file and getAll in models file

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,5 +1,7 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars');
 }
 
 const getById = () => {
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.text('vin').notNullable().unique();
+    tbl.text('make').notNullable();
+    tbl.text('model').notNullable();
+    tbl.decimal('mileage').notNullable();
+    tbl.text('title');
+    tbl.text('transmission');
+  });
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
