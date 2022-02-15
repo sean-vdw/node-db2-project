0c6c02ac543caa475520c82a3be720eca5950296
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-15 12:19:19 -0800

    completed first middlewares

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,68 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const Cars = require('./cars-model');
+const vinValidator = require('vin-validator');
+
+const checkCarId = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const car = await Cars.getById(id);
+    if (!car) {
+      res.status(404).json({ message: `car with id ${id} is not found`});
+    } else {
+      req.car = car;
+      next();
+    }
+  } catch(err) {
+    next(err);
+  };
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin, make, model, mileage } = req.body;
+  if (vin === undefined) {
+    res.status(400).json({ message: "vin is missing" });
+  } else if (make === undefined) {
+    res.status(400).json({ message: "make is missing" });
+  } else if (model === undefined) {
+    res.status(400).json({ message: "model is missing" });
+  } else if (mileage === undefined) {
+    res.status(400).json({ message: "mileage is missing" });
+  } else {
+    req.vin = vin;
+    req.make = make;
+    req.model = model;
+    req.mileage = mileage;
+    next();
+  };
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin } = req.body;
+  if (!vinValidator(vin)) {
+    res.status(400).json({ message: `vin ${vin} is invalid` });
+  } else {
+    req.vin = vin;
+    next();
+  };
+};
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  const { vin } = req.body;
+  try {
+    const carVin = await Cars.getByVin(vin);
+    if (carVin) {
+      res.status(400).json({ message: `vin ${vin} already exists` });
+    } else {
+      req.vin = vin;
+      next();
+    }
+  } catch(err) {
+    next(err);
+  };
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -8,6 +8,10 @@ const getById = (id) => {
   return db('cars').where('id', id).first();
 }
 
+const getByVin = (vin) => {
+  return db('cars').where('vin', vin).first();
+}
+
 const create = async car => {
   const [id] = await db('cars').insert(car);
   const newCar = await getById(id);
@@ -17,5 +21,6 @@ const create = async car => {
 module.exports = {
   getAll,
   getById,
+  getByVin,
   create
 }
